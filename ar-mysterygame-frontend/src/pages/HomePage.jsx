18ea9698cf2d1e872601da35a3685af627a5bd13
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-07-31 16:08:20 +0900

    自分の状態の表示

--- ar-mysterygame-frontend/src/pages/HomePage.jsx
+++ ar-mysterygame-frontend/src/pages/HomePage.jsx
@@ -2,15 +2,32 @@ import { useEffect, useState } from "react";
 import gameServer from "../network/gameServer";
 
 const HomePage = () => {
-    const [gameUser, setGameUser] = useState();
+    const [gameUser, setGameUser] = useState(null);
 
     useEffect(() => {
-        gameServer.get("/v1/users/hello", {userId: gameServer.getUserId()}, (response) => setGameUser(response.data));
+        gameServer.get("/v1/users/self", {userId: gameServer.getUserId()}, (response) => {
+            if (response == null) return
+            setGameUser(response.data);
+        });
     }, []);
 
     return (
         <>
             <h2>ログイン完了</h2>
+            <UserInfoView userInfo={gameUser} />
+        </>
+    );
+}
+
+const UserInfoView = (props) => {
+    const gameUser = props.userInfo;
+
+    if (gameUser == null) {
+        return <></>;
+    }
+
+    return (
+        <>
             <p>userId: {gameUser.userId}</p>
             <p>userName: {gameUser.userName}</p>
             <p>role: {gameUser.role}</p>

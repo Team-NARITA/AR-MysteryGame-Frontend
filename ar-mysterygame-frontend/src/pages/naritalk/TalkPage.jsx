df7f2deede23e0a218f7d49287cc96012f36e9f2
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-08-24 17:50:45 +0900

    トークページ wip

--- ar-mysterygame-frontend/src/pages/naritalk/TalkPage.jsx
+++ ar-mysterygame-frontend/src/pages/naritalk/TalkPage.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import gameServer from "../../network/gameServer";
 
-import { MainContainer, ChatContainer, MessageList, Message, TypingIndicator, MessageInput } from "@chatscope/chat-ui-kit-react";
+import { MainContainer, ChatContainer, MessageList, Message, TypingIndicator, MessageInput, Avatar } from "@chatscope/chat-ui-kit-react";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
 import Header from "../common/Header";
@@ -12,11 +12,40 @@ import AppArea from "../common/AppArea";
 const TalkPage = () => {
     const { chapterId } = useParams();
     const [ chapterData, setChapterData ] = useState();
+    const [ typingIndicator, setTypingIndicator ] = useState(null);
+    const [ chatLogs, setChatLogs ] = useState([]);
 
     useEffect(() => {
         gameServer.get("/v1/chapter/file/" + chapterId, [], (chapter) => {
             setChapterData(chapter.data);
         });
+        setChatLogs([
+            {
+                type: "message",
+                message: "こんにちは",
+                sender: "rain1208",
+                direction: "incomming"
+            },
+            {
+                type: "message",
+                message: "こんにちは",
+                sender: "rain1208",
+                direction: "outgoing"
+            },
+            {
+                type: "image",
+                image: "https://media.discordapp.net/attachments/1095617215291719710/1143782884428234852/001.png",
+                imageAlt: "image sample",
+                sender: "rain1208",
+                direction: "incomming"
+            },
+            {
+                type: "message",
+                message: "いい感じじゃない？",
+                sender: "rain1208",
+                direction: "outgoing"
+            }
+        ])
     }, [])
 
     return (
@@ -25,21 +54,10 @@ const TalkPage = () => {
             <AppArea>
                 <MainContainer responsive>
                     <ChatContainer>
-                        <MessageList>
-                            <Message model={{
-                                message: "こんにちは",
-                                sentTime: "15 mins ago",
-                                sender: "rain1208",
-                                direction: "incoming",
-                                position: "single"
-                            }} />
-                            <Message model={{
-                                message: "こんにちは",
-                                sentTime: "15 mins ago",
-                                sender: "rain1208",
-                                direction: "outgoing",
-                                position: "single"
-                            }} />
+                        <MessageList typingIndicator={typingIndicator}>
+                            {
+                                chatLogs.map((item, i) => (<ChatMessage key={i} chatLog={item} />))
+                            }
                         </MessageList>
                         <MessageInput attachButton={false} />
                     </ChatContainer>
@@ -48,6 +66,34 @@ const TalkPage = () => {
             <HomeButton />
         </>
     )
+}
+
+const ChatMessage = (props) => {
+    const chatLog = props.chatLog;
+    
+    switch (chatLog.type) {
+        case "message": 
+            return (
+                <Message model={{
+                    message: chatLog.message,
+                    sender: chatLog.sender,
+                    direction: chatLog.direction,
+                    position: "single"
+                }}>
+                    <Message.Header sender={chatLog.sender} />
+                </Message>
+            )
+        case "image":
+            return (
+                <Message model={{
+                    sender: chatLog.sender,
+                    direction: chatLog.direction
+                }}>
+                    <Message.Header sender={chatLog.sender} />
+                    <Message.ImageContent src={chatLog.image} alt={chatLog.imageAlt} width="70vw"/>
+                </Message>
+            )
+    }
 }
 
 export default TalkPage;

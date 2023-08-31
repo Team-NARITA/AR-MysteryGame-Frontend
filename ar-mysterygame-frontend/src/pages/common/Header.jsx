11eade84ca2b3364fd08607718c14ba4f1480f66
rain1208 <58714901+raink1208@users.noreply.github.com>
2023-08-31 12:00:05 +0900

    戻るボタンを追加

--- ar-mysterygame-frontend/src/pages/common/Header.jsx
+++ ar-mysterygame-frontend/src/pages/common/Header.jsx
@@ -1,9 +1,13 @@
-import "./Header.css"
+import "./Header.css";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const Header = (props) => {
     const imgSrc = props.imgSrc;
+    const prev = props.prev;
+
     return (
         <div className="header">
+            {prev ? <ArrowBackIosIcon className="prev-button" onClick={() => prev(-1)} /> : <></>}
             {imgSrc ? <img src={imgSrc} /> : <></>}
         </div>
     );

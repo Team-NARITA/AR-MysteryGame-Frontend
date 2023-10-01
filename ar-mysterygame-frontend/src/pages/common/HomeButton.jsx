3cbef7be213642d6bac79873356b3f3750e53139
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-10-02 03:07:19 +0900

    ホームボタン追加

--- ar-mysterygame-frontend/src/pages/common/HomeButton.jsx
+++ ar-mysterygame-frontend/src/pages/common/HomeButton.jsx
@@ -1,11 +1,18 @@
-import "./HomeButton.css"
+import { Link } from "react-router-dom";
+import "./HomeButton.css";
+
+import CircleIcon from '@mui/icons-material/Circle';
 
 const HomeButton = () => {
     return (
         <div className="footer">
             <div className="button-left"></div>
             <div className="wrap">
-                <div className="button-area"></div>
+                <div className="button-area">
+                    <Link to={"/"}>
+                        <CircleIcon style={{height:"100%", width:"100%", color:"#ddd"}}/>
+                    </Link>
+                </div>
             </div>
             <div className="button-right"></div>
         </div>

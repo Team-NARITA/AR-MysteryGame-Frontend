1b2ac1e08efb8ad90ff14706bf7704129ad26838
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-10-20 07:18:19 +0900

    wip

---
+++ ar-mysterygame-frontend/src/pages/CouponPage.jsx
@@ -0,0 +1,16 @@
+import AppArea from "./common/AppArea";
+import Header from "./common/Header";
+import HomeButton from "./common/HomeButton";
+
+const CouponPage = () => {
+    
+    return (
+        <>
+            <Header />
+            <AppArea>
+
+            </AppArea>
+            <HomeButton />
+        </>
+    );
+}
